da8c9360d67c244267a84f856bac5914d6a43206
Wisinewski <g_gomees@hotmail.com>
2021-06-07 20:24:11 -0300

    refactor: change makeDbLoadPersonById factory helper method

--- src/main/factories/usecases/load-person-by-id/db-load-person-by-id.ts
+++ src/main/factories/usecases/load-person-by-id/db-load-person-by-id.ts
@@ -1,8 +1,9 @@
+import { PersonDynamoRepository } from './../../../../infra/db/dynamodb/person-dynamo-repository';
 import { DbLoadPersonById } from '../../../../data/usecases/load-person-by-id/db-load-person-by-id';
-import { PersonMongoRepository } from '../../../../infra/db/mongodb/person-mongo-repository';
 import { LoadPersonById } from '../../../../domain/usecases/load-person-by-id';
 
 export const makeDbLoadPersonById = (): LoadPersonById => {
-  const personMongoRepository = new PersonMongoRepository()
-  return new DbLoadPersonById(personMongoRepository)
+  const table = process.env['PERSONS_TABLE']
+  const personDynamoRepository = new PersonDynamoRepository(table)
+  return new DbLoadPersonById(personDynamoRepository)
 }
