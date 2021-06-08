42b42fe2d0ad5d9e87504f34df31677b7ba521ad
Wisinewski <g_gomees@hotmail.com>
2021-06-08 10:29:34 -0300

    test: ensure PersonDynamoRepositorySpy implements AddPersonRepository protocol

--- src/infra/db/dynamodb/test/mock-dynamo-repository.ts
+++ src/infra/db/dynamodb/test/mock-dynamo-repository.ts
@@ -1,11 +1,18 @@
+import { AddPersonParams } from './../../../../domain/usecases/add-person';
+import { AddPersonRepository } from './../../../../data/protocols/db/add-person-repository';
 import { PersonModel } from './../../../../domain/models/person';
 import { LoadPersonByIdRepository } from './../../../../data/protocols/db/load-person-by-id-repository';
 
-export class PersonDynamoRepositorySpy implements LoadPersonByIdRepository {
+export class PersonDynamoRepositorySpy implements LoadPersonByIdRepository, AddPersonRepository {
   data: any
   result: any
   async loadById (id: string): Promise<PersonModel> { 
     this.data = id
+    return this.result
+  }
+
+  async add (personData: AddPersonParams): Promise<PersonModel> {
+    this.data = personData
     return this.result
   }
 }
