0fb12074fcbfe949aa1430f6033b1c4be9e618c8
Wisinewski <g_gomees@hotmail.com>
2021-06-08 10:52:45 -0300

    test: ensure PersonDynamoRepositorySpy implements DeletePersonByIdRepository protocol

--- src/infra/db/dynamodb/test/mock-dynamo-repository.ts
+++ src/infra/db/dynamodb/test/mock-dynamo-repository.ts
@@ -1,9 +1,10 @@
+import { DeletePersonByIdRepository } from './../../../../data/protocols/db/delete-person-by-id-repository';
 import { AddPersonParams } from './../../../../domain/usecases/add-person';
 import { AddPersonRepository } from './../../../../data/protocols/db/add-person-repository';
 import { PersonModel } from './../../../../domain/models/person';
 import { LoadPersonByIdRepository } from './../../../../data/protocols/db/load-person-by-id-repository';
 
-export class PersonDynamoRepositorySpy implements LoadPersonByIdRepository, AddPersonRepository {
+export class PersonDynamoRepositorySpy implements LoadPersonByIdRepository, AddPersonRepository, DeletePersonByIdRepository {
   data: any
   result: any
   async loadById (id: string): Promise<PersonModel> { 
@@ -13,6 +14,11 @@ export class PersonDynamoRepositorySpy implements LoadPersonByIdRepository, AddP
 
   async add (personData: AddPersonParams): Promise<PersonModel> {
     this.data = personData
+    return this.result
+  }
+
+  async deleteById (id: string): Promise<boolean> {
+    this.data = id
     return this.result
   }
 }
