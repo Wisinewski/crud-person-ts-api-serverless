9e9748be09d812b6060fa61f0a6a49b42493211f
Wisinewski <g_gomees@hotmail.com>
2021-06-09 17:50:47 -0300

    refactor: remove unused files

--- src/main/factories/controllers/load-person-by-id/load-person-by-id-validation-factory.ts
+++
@@ -1,8 +0,0 @@
-import { MongoIdValidation } from './../../../../validation/validators/mongo-id-validation';
-import { Validation } from './../../../../presentation/protocols/validation';
-import { ValidationComposite } from './../../../../validation/validators/validation-composite';
-
-export const makeLoadPersonByIdValidation = (): ValidationComposite => {
-  const validations: Validation[] = []
-  return new ValidationComposite(validations)
-}
