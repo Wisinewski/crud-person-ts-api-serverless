c245e81cf541ded0c2f781bae10ecabda4b79ecd
Wisinewski <g_gomees@hotmail.com>
2021-06-10 18:17:15 -0300

    feat: ensure DbAddPerson receives UuidGeneratorAdapter as dependency

--- src/data/usecases/add-person/db-add-person.ts
+++ src/data/usecases/add-person/db-add-person.ts
@@ -1,3 +1,4 @@
+import { UuidGeneratorAdapter } from './../../../infra/uuid/uuid-generator';
 import { PersonModel } from './../../../domain/models/person';
 import { LoadPersonByCpfRepository } from './../../protocols/db/load-person-by-cpf-repository';
 import { AddPersonRepository } from './../../protocols/db/add-person-repository';
@@ -6,12 +7,14 @@ import { AddPerson, AddPersonParams } from './../../../domain/usecases/add-perso
 export class DbAddPerson implements AddPerson {
   constructor (
     private readonly loadPersonByCpfRepository: LoadPersonByCpfRepository,
-    private readonly addPersonRepository: AddPersonRepository
+    private readonly addPersonRepository: AddPersonRepository,
+    private readonly uuidGeneratorAdapter: UuidGeneratorAdapter
   ) {}
 
   async add (personData: AddPersonParams): Promise<PersonModel> {
     const person = await this.loadPersonByCpfRepository.loadByCpf(personData.cpf)
     if (!person) {
+      personData['id'] = this.uuidGeneratorAdapter.generate()
       const person = await this.addPersonRepository.add(personData)
       return person
     }
