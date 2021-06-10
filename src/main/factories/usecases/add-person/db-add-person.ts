dff9b15222779eb77251ddf4bb1278f1bd8a8ab7
Wisinewski <g_gomees@hotmail.com>
2021-06-10 18:19:21 -0300

    feat: create add, delete, load by filter and update data layer factories

--- src/main/factories/usecases/add-person/db-add-person.ts
+++ src/main/factories/usecases/add-person/db-add-person.ts
@@ -1,8 +1,10 @@
-import { PersonMongoRepository } from './../../../../infra/db/mongodb/person-mongo-repository';
+import { UuidGeneratorAdapter } from './../../../../infra/uuid/uuid-generator';
+import { PersonDynamoRepository } from './../../../../infra/db/dynamodb/person-dynamo-repository';
 import { DbAddPerson } from './../../../../data/usecases/add-person/db-add-person';
 import { AddPerson } from './../../../../domain/usecases/add-person';
 
 export const makeDbAddPerson = (): AddPerson => {
-  const personMongoRepository = new PersonMongoRepository()
-  return new DbAddPerson(personMongoRepository, personMongoRepository)
+  const personDynamoRepository = new PersonDynamoRepository()
+  const uuidGeneratorAdapter = new UuidGeneratorAdapter()
+  return new DbAddPerson(personDynamoRepository, personDynamoRepository, uuidGeneratorAdapter)
 }
--- src/main/factories/usecases/load-person-by-filter/db-load-person-by-filter.ts
+++ src/main/factories/usecases/load-person-by-filter/db-load-person-by-filter.ts
@@ -1,8 +1,8 @@
+import { PersonDynamoRepository } from './../../../../infra/db/dynamodb/person-dynamo-repository';
 import { DbLoadPersonByFilter } from './../../../../data/usecases/load-person-by-filter/db-load-person-by-filter';
-import { PersonMongoRepository } from './../../../../infra/db/mongodb/person-mongo-repository';
 import { LoadPersonByFilter } from './../../../../domain/usecases/load-person-by-filter';
 
 export const makeDbLoadPersonByFilter = (): LoadPersonByFilter => {
-  const personMongoRepository = new PersonMongoRepository()
-  return new DbLoadPersonByFilter(personMongoRepository)
+  const personDynamoRepository = new PersonDynamoRepository()
+  return new DbLoadPersonByFilter(personDynamoRepository)
 }
