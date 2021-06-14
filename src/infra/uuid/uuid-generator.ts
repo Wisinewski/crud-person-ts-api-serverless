aebca4cd7fe0057583e99f7ebdb5d237defce2ac
Wisinewski <g_gomees@hotmail.com>
2021-06-14 13:14:28 -0300

    feat: ensure UuidGenerator returns an uuid if geenrator return an uuid

--- src/infra/uuid/uuid-generator.ts
+++ src/infra/uuid/uuid-generator.ts
@@ -1,8 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
 import { UuidGeneration } from './../../presentation/protocols/uuid-generation';
+import { v4 as uuidv4 } from 'uuid'
 
 export class UuidGeneratorAdapter implements UuidGeneration {
   generate (): string {
-    return uuidv4()
+    return Date.now().toString()
+    //return uuidv4()
   }
 }
